6d5c9674056b9e8665acc4f6239add2550b284d1
Tanmay Kakulte <121185410+tanmay23here@users.noreply.github.com>
2023-09-07 15:42:46 +0530

    Update ImageGen.jsx

--- src/components/ImageGen.jsx
+++ src/components/ImageGen.jsx
@@ -1,19 +1,16 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const ImageGenerator = () => {
   const [input, setInput] = useState('');
-  const [seed, setSeed] = useState('');
-  const [steps, setSteps] = useState(10);
-  const [guidanceScale, setGuidanceScale] = useState(0.5);
-  const [width, setWidth] = useState(500);
-  const [height, setHeight] = useState(300);
   const [image, setImage] = useState(null);
-  const [loading, setLoading] = useState(false)
-  const API_TOKEN = "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";
+  const [loading, setLoading] = useState(false);
+  const [seed, setSeed] = useState('');
+  const API_TOKEN = process.env.REACT_APP_API_TOKEN;
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setImage(null)
-    setLoading(true)
+    setImage(null);
+    setLoading(true);
     const newSeed = Math.floor(Math.random() * 1000000) + 1;
     const res = await fetch('https://api-inference.huggingface.co/models/prompthero/openjourney', {
       method: 'POST',
@@ -23,15 +20,11 @@ const ImageGenerator = () => {
       },
       body: JSON.stringify({
         inputs: input,
-        steps,
-        guidance_scale: guidanceScale,
         seed: newSeed,
-        width,
-        height,
       }),
     });
     const blob = await res.blob();
-    setLoading(false)
+    setLoading(false);
     setImage(window.URL.createObjectURL(blob));
   };
 
@@ -44,106 +37,39 @@ const ImageGenerator = () => {
 
   return (
     <div style={styles.container}>
-      <h1 style={styles.heading}><label className='form-label'>Image</label> <label style={styles.gen}>Generator</label></h1>
-      <form onSubmit={handleSubmit} style={styles.form}>
-        <div style={styles.row}>
-          <input
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            style={styles.input}
-            placeholder="Enter your text"
-          />
-        </div>
-        <div style={styles.row}>
-          <div style={styles.sliderContainer}>
-            <label className="form-label">
-              Steps: <input type='number' className='values' value={steps}
-              onChange={(e) => setSteps(Number(e.target.value))}/>
-              <input
-                type="range"
-                value={steps}
-                onChange={(e) => setSteps(Number(e.target.value))}
-                style={styles.rangeInput}
-                min={1}
-                max={500}
-              />
-            </label>
+      <div style={styles.container}>
+        <h1 style={styles.heading}>
+          <label className='form-label'>Image</label> <label style={styles.gen}>Generator</label>
+        </h1>
+        <form onSubmit={handleSubmit} style={styles.form}>
+          <div style={styles.row}>
+            <input
+              type="text"
+              value={input}
+              className='Prompt'
+              onChange={(e) => setInput(e.target.value)}
+              style={{ ...styles.input, width: '100%' }}
+              placeholder="Enter Your Prompt"
+            />
           </div>
-          <div style={styles.sliderContainer}>
-            <label className="form-label">
-              Guidance Scale: <input type='number' className='values' value={guidanceScale}
-              onChange={(e) => setGuidanceScale(Number(e.target.value))}/>
-              <input
-                type="range"
-                value={guidanceScale}
-                onChange={(e) => setGuidanceScale(Number(e.target.value))}
-                style={styles.rangeInput}
-                step={0.1}
-                min={1}
-                max={20}
-              />
-            </label>
-          </div>
-        </div>
-        <div style={styles.row}>
-          <div style={styles.sliderContainer}>
-            <label className="form-label">
-              Width: <input type='number' className='values' value={width}
-              onChange={(e) => setWidth(Number(e.target.value))}/>
-              <input
-                type="range"
-                value={width}
-                onChange={(e) => setWidth(Number(e.target.value))}
-                style={styles.rangeInput}
-                min={128}
-                max={1024}
-              />
-            </label>
-          </div>
-          <div style={styles.sliderContainer}>
-            <label className="form-label">
-              Height:<input type='number' className='values' value={height}
-              onChange={(e) => setHeight(Number(e.target.value))}/>
-              <input
-                type="range"
-                value={height}
-                onChange={(e) => setHeight(Number(e.target.value))}
-                style={styles.rangeInput}
-                min={128}
-                max={1024}
-              />
-            </label>
-          </div>
-        </div>
-        <div style={styles.row}>
-          <input
-            type="number"
-            value={seed}
-            onChange={(e) => setSeed(e.target.value)}
-            style={styles.input}
-            placeholder="Enter seed"
-            min={0}
-          />
-        </div>
-        <div style={styles.row}>
-          <button type="submit" className='btn' style={styles.button}>
-            {
-              loading ? "Loading.." : "Generate"
-            }
-          </button>
-        </div>
-      </form>
-      {image && (
-        <div style={styles.imageContainer}>
-          <img src={image} alt="Generated" style={styles.image} />
-          <div style={styles.downloadContainer}>
-            <button onClick={handleDownload} className='btn' style={styles.downloadButton}>
-              Download
+          <div style={styles.row} value={seed} onChange={(e) => setSeed(e.target.value)}></div>
+          <div style={styles.row}>
+            <button type="submit" className='btn' style={styles.button}>
+              {loading ? "Loading.." : "Generate"}
             </button>
           </div>
-        </div>
-      )}
+        </form>
+        {image && (
+          <div style={styles.imageContainer} className='btn1'>
+            <img src={image} alt="Generated" style={styles.image} />
+            <div style={styles.downloadContainer}>
+              <button onClick={handleDownload} className='btn' style={styles.downloadButton}>
+                Download
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
@@ -169,10 +95,11 @@ const styles = {
     flexDirection: 'column',
     alignItems: 'stretch',
     marginBottom: '2rem',
+    width: '35rem',
   },
   row: {
     display: 'flex',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginBottom: '1rem',
   },
   input: {
@@ -180,44 +107,41 @@ const styles = {
     fontSize: '1rem',
     border: '1px solid #ccc',
     borderRadius: '0.5rem',
-    width: '100%',
   },
-  rangeInput: {
-    width: '100%',
-    marginBottom: '0.5rem',
+  button: {
+    padding: '0.5rem 1rem',
+    backgroundColor: '#ffdd02',
+    border: 'none',
+    borderRadius: '0.5rem',
+    fontSize: '1rem',
+    cursor: 'pointer',
   },
-  sliderContainer: {
-    flex: '1',
-    marginRight: '1rem',
-  },
-  gen:{
-    color: '#7559ff',
-  },
-
   imageContainer: {
-    width: '100%',
+    marginTop: '2rem',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    marginTop: '2rem',
-    marginRight: '1rem',
   },
   image: {
     maxWidth: '100%',
-    height: 'auto',
-    marginBottom: '1rem',
+    maxHeight: '500px',
+    border: '1px solid white',
   },
   downloadContainer: {
-    marginRight: '9.5rem',
+    display: 'flex',
+    justifyContent: 'center',
   },
   downloadButton: {
-    padding: '0.5rem',
-    fontSize: '1rem',
+    padding: '0.5rem 1rem',
+    backgroundColor: '#ffdd02',
     border: 'none',
     borderRadius: '0.5rem',
-    backgroundColor: '#0077ff',
-    color: '#fff',
+    fontSize: '1rem',
     cursor: 'pointer',
+    marginTop: '1rem',
+  },
+  gen: {
+    color: '#ffdd02',
   },
 };
 
